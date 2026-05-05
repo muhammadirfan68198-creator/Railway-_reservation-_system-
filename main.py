6b5ad8e7471c44cf3591000d6e8d06eacfd63710
MUHAMMAD IRFAN I <muhammadirfan68198@gmail.com>
2026-05-05 16:10:01 +0530

    Added menu system

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print ("Railway Reservation System ")
+seats = 50
+bookings = {}
+booking_id = 1
+
+while True:
+    print("\n--- Railway Reservation System ---")
+    print("1. Check Availability")
+    print("2. Book Ticket")
+    print("3. View Ticket")
+    print("4. Cancel Ticket")
+    print("5. Exit")
+
+    choice = input("Enter your choice: ")
